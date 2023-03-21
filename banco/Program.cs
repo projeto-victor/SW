6286f25fa211ba1a7e46fb590bd2fd8035f66ed2
Victor Marques <100208584+projeto-victor@users.noreply.github.com>
2023-03-21 12:32:44 -0300

    Add files via upload

---
+++ banco/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace banco;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Banco Etec MCM");
+        conta c = new conta();
+        //c.Saldo = 1500;
+        c.Depositar(1500);
+        //c.Limite = 500;
+        c.AjustaLimite(500);
+
+         
+
+
+        //Console.WriteLine("Seu saldo é de: " + c.Saldo + "e seu
+        // limite é de: " + c.Limite);
+        Console.WriteLine("Seu saldo é de: " + c.MostrarSaldo());
+    }
+}
