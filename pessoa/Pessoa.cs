a39e63392a708304bea9ef76b9585f0d4358df73
Victor Marques <100208584+projeto-victor@users.noreply.github.com>
2023-03-28 12:32:54 -0300

    Add files via upload

---
+++ pessoa/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿namespace pessoa;
+class Pessoa
+{
+    public Pessoa (){
+        this.Nome = "Não Informado";
+        this.SobreNome = "Não Informado";
+        this.Idade = 0;
+    }
+
+        public Pessoa (string nome, int idade){
+        this.Nome = nome;
+        this.SobreNome = "Não Informado";
+        this.Idade = idade;
+    }
+
+    
+    private string? Nome {get; set;}  
+
+    private string? SobreNome {get;set;}
+
+    private int Idade {get;set;}
+
+
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    } 
+
+    public void AlteraSobreNome(string sobranome){
+        this.SobreNome = sobranome;
+    }
+
+    public void AlteraIdade(int idade){
+        this.Idade = idade;
+    }
+
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + this.Nome);
+        Console.WriteLine("SobreNome: " + this.SobreNome);
+        Console.WriteLine("Idade: " + this.Idade);
+    }
+
+
+}
---
+++ pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Godofredo", 18);
+        p1.AlteraNome("Victor");
+        p1.AlteraSobreNome("Marques");
+        p1.AlteraIdade(16);
+        p1.MostraDados();
+    }
+}
