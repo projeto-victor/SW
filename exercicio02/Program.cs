368369d9d3c69d748b1f135d34ce719cf8397159
Victor Marques <100208584+projeto-victor@users.noreply.github.com>
2023-05-22 08:00:15 -0300

    Add files via upload

---
+++ exercicio02/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace exercicio02;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contrato c = new Contrato();
+        ContratoPessoaFisica cf = new ContratoPessoaFisica();
+        ContratoPessoaJuridica cj = new ContratoPessoaJuridica();
+
+        cf.Nome = "Vitu";
+        cf.Email = "Vitotoso@gmail.com";
+        cf.Telefone = "(11)99999-9999";
+        cf.Idade = 16;
+        cf.Cpf = 454545454-54;
+        cj.Nome = "Jeferçon";
+        cj.Email = "jeferçon@gmail.com";
+        cj.Telefone = "(11)88888-8888";
+        cj.Cnpj = 01010101010101;
+        cj.Inscricao = 212121212121;
+
+        Console.WriteLine(cf.MostraDados());
+        Console.WriteLine(cj.MostraDados());
+    }
+} 
+
